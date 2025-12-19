9d9277dc8c4faf076b8d2564b4a4338144826f6a
Kamil Synowiec <kamilsynowiec18@gmail.com>
2025-12-19 13:54:59 +0100

    First commit to cpp file

---
+++ ToDo/todo.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "todo.h"
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+
+
+using namespace std;
+
+int TodoManager::nextId = 0;
+
+int Task::getId() const
+{
+    return id;
+}
+
+string Task::getTitle() const
+{
+    return title;
+}
+
+bool Task::isCompleted() const
+{
+    if(status == true)
+        return true;
+    else
+        return false;
+}
+
+void Task::display(int id) const
+{
+    int prio = priority;
+    cout<<"ID: "<<id<<endl;
+    cout<<"Title: "<<title<<endl;
+    cout<<"Description: "<<description<<endl;
+    switch (prio)
+    {
+    case 0:
+        cout<<"Priority: High"<<endl;
+        break;
+    case 1:
+         cout<<"Priority: Mid"<<endl; 
+    case 2:
+        cout<<"Priority: LOW"<<endl;
+    default:
+        break;
+    }
+    if(isCompleted() == false)
+        cout<<"Status: X"<<endl;
+    else
+        cout<<"Status: Done!"<<endl;
+
+}
+
+bool TodoManager::addTask(Priority priority)
+{
+    string title, description;
+    cout<<"Set title: ";
+    getline(cin, title);
+
+    cout<<"Set description: ";
+    getline(cin, description);
+
+    if(title.empty())
+    {
+       throw TaskException("Title cannot be empty!");
+    }
+
+    if(description.empty())
+    {
+        throw TaskException("Description cannot be empty!");
+    }
+
+    int id = nextId++;
+
+    tasks.emplace_back(id, title, description, priority, false);
+    
+    return true;
+         
+}
+
+bool TodoManager::removeTask(int id)
+{
+    for(auto it = tasks.begin(); it != tasks.end(); ++it)
+    {
+        if(it->getId() == id)
+        {
+            cout<<"Found task: "<<it->getTitle()<<"do you watn remove it ?"<<endl;
+            string answer;
+            getline(cin, answer);
+            transform(answer.begin(), answer.end(), answer.begin(), ::tolower);
+            if(answer != "yes")
+            {
+                return false;
+            }
+            tasks.erase(it);
+            return true;
+        }
+    }
+    return false;
+}
+
+void edut
+
