d869753e3c0822b953602ee47c033c55a40f8c75
Kamil Synowiec <kamilsynowiec18@gmail.com>
2025-12-19 12:49:36 +0100

    Headers File

--- ToDo/todo.h
+++ ToDo/todo.h
@@ -1,22 +1,48 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+enum Priority { High, Mid, Low};
+
 class Task 
 {
+    
     private:
     int id;
-    enum Priority { High, Mid, Low};
+    string title;
     string description;
     Priority priority;
     bool status;
     public:
-    Task(int i, string desc, Priority prio, bool st) :id(i), description(desc), priority(prio), status(st) {};
-    bool addTask();
-    bool removeTask(int id);
-    void showAllTasks();
-    void markADone();
-    void display();
-    void getTask(int id);
+    Task(int i, string ti, string desc, Priority prio, bool st = false) :id(i), title(ti), description(desc), priority(prio), status(st) {};
+    void display(int id) const;
+    int getId() const;
+    string getTitle() const;
+    bool isCompleted() const;
 };
 
+class TodoManager
+{
+    private:
+    static int nextId;
+    vector<Task> tasks;
+    public:
+    bool addTask(string title, Priority priority);
+    bool removeTask(int id);
+    void editTask(int id);
+    void showAllTasks();
+    void toggleTask(int id);
+    void findTasksByTitle(const string& title);
+    void sortByPriority();
+
+};
+
+class FileStorage
+{
+    public: 
+    string filename;
+    void save(const vector<Task>& tasks);
+    void load(vector<Task>& tasks);
+
+};
