1bb2de7eff10143dafda9575358148d4989a97f0
Kamil Synowiec <kamilsynowiec18@gmail.com>
2026-01-21 14:16:52 +0100

    New Tests

--- Wzorce Projektowe/testyWzorce.cpp
+++ Wzorce Projektowe/testyWzorce.cpp
@@ -39,5 +39,66 @@ TEST_F(PayTest, TestPlatnoscBlikiem)
     EXPECT_TRUE(output.find("Zaplac Blikiem!") != string::npos);
 }
 
+TEST_F(PayTest, TestPlatnoscPrzelewem)
+{
+    testing::internal::CaptureStdout();
+    przelew->payment();
+    string output = testing::internal::GetCapturedStdout();
+    EXPECT_TRUE(output.find("Platnosc numer: 3") != string::npos);
+    EXPECT_TRUE(output.find("Zaplac przelewem!") != string::npos);
+}
+
+TEST_F(PayTest, TestPlatnoscRaty)
+{
+    testing::internal::CaptureStdout();
+    raty->payment();
+    string output = testing::internal::GetCapturedStdout();
+    EXPECT_TRUE(output.find("Platnosc numer: 4") != string::npos);
+    EXPECT_TRUE(output.find("Zaplac w ratach") != string::npos);
+}
+
+struct PayTestData {
+    unique_ptr<HowPay> paymentSystem;
+    string expected_output;
+};
 
 
+class PaySystemParamTest : public testing::TestWithParam<pair<string, string>> {};
+
+TEST_P(PaySystemParamTest, CorrectMessage)
+{
+    auto[strategyType, message] = GetParam();
+    
+    unique_ptr<HowPay> paymentSystem;
+    if(strategyType == "Card") paymentSystem = make_unique<PayByCard>();
+    else if(strategyType == "Blik") paymentSystem = make_unique<PayByBlik>();
+    else if(strategyType == "Transfer") paymentSystem = make_unique<PayByTransfer>();
+    else if(strategyType == "Installments") paymentSystem = make_unique<PayByInstallments>();
+
+    PaySystem system(move(paymentSystem), 1, "Testowa");
+
+    stringstream buffer;
+    streambuf* old = cout.rdbuf(buffer.rdbuf()); //pobierasz adres bufora Twojej zmienne  i ustawiasz ten bufor jako cel dla std::cout
+
+    system.payment();
+    
+    cout.rdbuf(old); //stan pierwotny
+    string output = buffer.str();
+
+    cout << "DEBUG: Przechwycony tekst to: [" << output << "]" << std::endl;
+
+    EXPECT_NE(output.find(message), string::npos);
+
+
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    PaymentMethods,
+    PaySystemParamTest,
+    ::testing::Values(
+        make_pair("Card", "Zaplac karta!"),
+        make_pair("Blik", "Zaplac Blikiem!"),
+        make_pair("Transfer", "Zaplac przelewem!"),
+        make_pair("Installments", "Zaplac w ratach")
+    )
+);
