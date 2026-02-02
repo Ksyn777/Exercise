39486c292806744288b1a51090dbd6ef52527860
Kamil Synowiec <kamilsynowiec18@gmail.com>
2026-02-02 18:23:43 +0100

    Add function

--- BoosAsio/czatServer.cpp
+++ BoosAsio/czatServer.cpp
@@ -3,85 +3,165 @@
 #include <vector>
 #include <thread>
 #include <memory>
+#include <mutex>
+#include <algorithm>
 
 using boost::asio::ip::tcp;
 using namespace std;
 
+// Struktura przechowująca dane klienta
 struct Client {
     string nick;
     shared_ptr<tcp::socket> socket;
+    string room;
 };
 
+// Globalne zmienne
 vector<Client> clients;
+mutex clients_mutex;
 boost::asio::io_context io;
+
+// Funkcja usuwająca klienta z listy po rozłączeniu
+void remove_client(shared_ptr<tcp::socket> socket) {
+    lock_guard<mutex> lock(clients_mutex);
+    clients.erase(remove_if(clients.begin(), clients.end(),
+        [&socket](const Client& c) { return c.socket == socket; }),
+        clients.end());
+}
 
 void handle_client(Client client) {
     try {
         char data[1024];
         while (true) {
-            //Obiekt, który przechowa informację, czy operacja czytania się udała
             boost::system::error_code error;
-            //Próba odczytania danych z gniazda (socketu). Program zatrzymuje się tu i czeka, aż klient coś wyśle.
+            // Oczekiwanie na dane od klienta
             size_t len = client.socket->read_some(boost::asio::buffer(data), error);
 
-            //Jeśli błąd to eof (End Of File), oznacza to, że klient zamknął aplikację. Przerywamy pętlę.
             if (error == boost::asio::error::eof) {
-                cout << client.nick << " rozlaczony.\n";
+                cout << "[System] " << client.nick << " rozlaczony.\n";
                 break;
-            //Jeśli wystąpił jakikolwiek inny błąd sieciowy, wypisujemy go i kończymy obsługę.
             } else if (error) {
-                cout << "Blad klienta " << client.nick << ": " << error.message() << endl;
+                cout << "[Blad] " << client.nick << ": " << error.message() << endl;
                 break;
             }
 
-            //Zamiana surowych bajtów z bufora na obiekt typu string o długości len
             string message(data, len);
-            //Doklejenie nicku nadawcy do treści wiadomości.
-            string formatted = client.nick + ": " + message;
+            // Usuwanie znaków nowej linii z odebranej wiadomości dla łatwiejszego parsowania
+            message.erase(remove(message.begin(), message.end(), '\n'), message.end());
+            message.erase(remove(message.begin(), message.end(), '\r'), message.end());
 
-            // wyslij do wszystkich pozostalych klientow
-            for (auto& c : clients) {
-                //Sprawdzenie, aby nie wysyłać wiadomości z powrotem do osoby, która ją napisała.
-                if (c.socket != client.socket) {
-                    //Wysłanie sformatowanej wiadomości do gniazda innego klienta.
-                    boost::asio::write(*c.socket, boost::asio::buffer(formatted));
+            // KOMENDA: /join <pokoj>
+            if (message.substr(0, 6) == "/join ") {
+                string new_room = message.substr(6);
+                
+                lock_guard<mutex> lock(clients_mutex);
+                for (auto& c : clients) {
+                    if (c.socket == client.socket) {
+                        c.room = new_room;
+                        client.room = new_room; // Aktualizacja lokalnej kopii
+                        break;
+                    }
+                }
+                string system_msg = "Serwer: Jestes teraz w pokoju: " + new_room + "\n";
+                boost::asio::write(*client.socket, boost::asio::buffer(system_msg));
+                continue;
+            }
+
+            // KOMENDA: /who
+            if (message == "/who") {
+                string list_msg = "Osoby w pokoju [" + client.room + "]: ";
+                {
+                    lock_guard<mutex> lock(clients_mutex);
+                    for (auto& c : clients) {
+                        if (c.room == client.room) list_msg += c.nick + " ";
+                    }
+                }
+                list_msg += "\n";
+                boost::asio::write(*client.socket, boost::asio::buffer(list_msg));
+                continue;
+            }
+
+            // KOMENDA: /lobby
+            if(message == "/lobby")
+            {
+                string old_room = client.room;
+                string new_room = "lobby";
+                {
+                    lock_guard<mutex> lock(clients_mutex);
+                    for (auto& c : clients) {
+                        if (c.socket == client.socket) {
+                            c.room = new_room;
+                            client.room = new_room; // Aktualizacja lokalnej kopii wątku
+                            break;
+                        }
+                    }
+                }
+                string system_msg = "Serwer: Wrociles do lobby.\n";
+                boost::asio::write(*client.socket, boost::asio::buffer(system_msg));
+
+                string leave_notify = "[System] " + client.nick + " opuscil pokoj i wrocil do lobby.\n";
+                lock_guard<mutex> lock(clients_mutex);
+                for (auto& c : clients) {
+                    if (c.room == old_room && c.socket != client.socket) {
+                        boost::system::error_code ec;
+                        boost::asio::write(*c.socket, boost::asio::buffer(leave_notify), ec);
+                    }
+                }
+                continue;
+            }
+
+            // ZWYKŁA WIADOMOŚĆ - Wysyłka do osób w tym samym pokoju
+            string formatted = "[" + client.room + "] " + client.nick + ": " + message + "\n";
+            {
+                lock_guard<mutex> lock(clients_mutex);
+                for (auto& c : clients) {
+                    // Nie wysyłaj do siebie i sprawdzaj pokój
+                    if (c.socket != client.socket && c.room == client.room) {
+                        boost::system::error_code err;
+                        boost::asio::write(*c.socket, boost::asio::buffer(formatted), err);
+                    }
                 }
             }
         }
-    //Złapanie i wypisanie ewentualnych wyjątków standardowych.
     } catch (exception& e) {
-        cout << "Wyjatek klienta " << client.nick << ": " << e.what() << endl;
+        cout << "[Wyjatek] " << client.nick << ": " << e.what() << endl;
     }
+
+    // Sprzątanie po wyjściu z pętli
+    remove_client(client.socket);
 }
 
 int main() {
-    tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), 9000));
-    cout << "Serwer nasluchuje na porcie 9000...\n";
+    try {
+        tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), 9000));
+        cout << "Serwer czatu uruchomiony na porcie 9000...\n";
 
-    //Główna pętla serwera akceptująca nowych klientów.
-    while (true) {
-        //Stworzenie nowego, „inteligentnego” wskaźnika na gniazdo dla przyszłego klienta.
-        auto sock = make_shared<tcp::socket>(io);
-        //Program zatrzymuje się tutaj i czeka, aż ktoś spróbuje się połączyć. 
-        //Gdy to nastąpi, sock zostaje powiązany z tym połączeniem.
-        acceptor.accept(*sock);
+        while (true) {
+            auto sock = make_shared<tcp::socket>(io);
+            acceptor.accept(*sock);
 
-        // odczytaj nick
-        char buffer[256];
-        //Odczytanie nicku od klienta zaraz po połączeniu.
-        size_t len = sock->read_some(boost::asio::buffer(buffer));
-        //Konwersja odebranego nicku na tekst.
-        string nick(buffer, len);
+            // Pierwsza wiadomość od klienta to jego nick
+            char buffer[256];
+            size_t len = sock->read_some(boost::asio::buffer(buffer));
+            string nick(buffer, len);
+            nick.erase(remove(nick.begin(), nick.end(), '\n'), nick.end());
+            nick.erase(remove(nick.begin(), nick.end(), '\r'), nick.end());
 
-        //Stworzenie struktury/obiektu klienta łączącego nick z jego socketem.
-        Client client{nick, sock};
-        clients.push_back(client);
-        cout << "Polaczono klienta: " << nick << endl;
+            // Tworzenie nowego klienta (domyślnie pokój "lobby")
+            Client new_client{nick, sock, "lobby"};
+            
+            {
+                lock_guard<mutex> lock(clients_mutex);
+                clients.push_back(new_client);
+            }
 
-        // uruchom watek dla tego klienta
-        thread t(handle_client, client);
-        //Odłączenie wątku. Serwer nie musi czekać, aż wątek się skończy
-        t.detach();
+            cout << "[Polaczenie] " << nick << " dołączył do lobby.\n";
+
+            // Start wątku obsługi
+            thread(handle_client, new_client).detach();
+        }
+    } catch (exception& e) {
+        cerr << "[Fatal Error] " << e.what() << endl;
     }
 
     return 0;
